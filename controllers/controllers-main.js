1d61fe7da5eebf44057450551fb92ea26e58ce22
Kathryn Zaharek <kathrynzaharek@Kathryns-MacBook-Pro.local>
2019-11-05 14:36:21 -0500

    Added form and other  html to new.ejs and routes to support. Added style to link on landing page. Added link from mainSearch to mainNew. All tested and working.

--- controllers/controllers-main.js
+++ controllers/controllers-main.js
@@ -13,40 +13,50 @@ router.get('/entryData', (req,res) => {
         if (err) {
             res.send(err)
         } else {
-            res.redirect('/HP_Encyclopedia/mainSearch')
+            res.redirect('/HP_Encyclopedia/mainContent/mainSearch')
         }
     });
 })
 
-//Landing
+//Landing page
 router.get('/', (req,res) => {
     console.log("landing page - check!")
     res.render('mainContent/landing.ejs');
 })
 
-// //Index
-// router.get('/mainSearch', (req,res) => {
-//     Items.find({}, (err, items) => {
-//         if (err) {
-//             res.send(err)
-//         } else {
-//             console.log('These items')
-//             res.render('mainSearch.ejs', {
-//                 allItems: items
-//             })
-//         }
-//     })
-// })
-
+//Index / mainSearch page
+router.get('/mainContent/mainSearch', (req,res) => {
+    Items.find({}, (err, items) => {
+        if (err) {
+            res.send(err)
+        } else {
+            console.log('Main search page - check!')
+            res.render('mainContent/mainSearch.ejs', {
+                allEntries: items
+            })
+        }
+    })
+})
 
 //New
-
+router.get('/mainContent/mainNew', (req,res) => {
+    res.render('mainContent/mainNew.ejs');
+})
 
 //Show
 
 
 //Create 
-
+router.post('/mainContent/mainSearch', (req,res) => {
+    Items.create(req.body, (err, newEntry) => {
+        if (err) {
+            res.send(err)
+        } else {
+            console.log('New items')
+        res.redirect('/HP_Encyclopedia/mainContent/mainSearch')
+        }
+    });
+});
 
 
 //Delete
