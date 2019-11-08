a89030ca0d96eed8599384bc0ed5abc160ae666f
Kathryn Zaharek <kathrynzaharek@Kathryns-MacBook-Pro.local>
2019-11-08 16:38:00 -0500

    added to server.js

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-const express = require('express')
+const PORT = process.env.PORT || 3000;
+const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const methodOverride = require('method-override');
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/hpencyclopedia'
 
 //Controller
 const mainController = require('./controllers/controllers-main.js');
 
 //Setup DB
-mongoose.connect('mongodb://localhost:27017/hpencyclopedia', {
+
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true });
 mongoose.connection.once('open', () => {
     console.log('connected to mongo')
@@ -20,4 +23,4 @@ app.use(methodOverride('_method'));
 app.use('/HP_Encyclopedia', mainController);
 
 
-app.listen(3000, (req, res) => console.log('listening on PORT 3000!'))
+app.listen(PORT, (req, res) => console.log('listening on PORT 3000!'))
